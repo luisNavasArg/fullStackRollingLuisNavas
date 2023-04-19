da368a3f566a445ffd80b7b471d73f72bb13988f
luisNavasArg <lpipnavas@gmail.com>
2023-04-18 21:42:22 -0300

    Intro aJavascript

---
+++ javascript/clase1/js/app.js
@@ -0,0 +1,49 @@
+//para mostrar en la consola y verificar que anda
+//console.log('Estamos conectados al javascript');
+//Esto es para mostrar una ventana emergente 
+//alert("Mensaje a envíar");
+
+//Variables
+//declaración de las variables 
+//string  
+let nombre="Luis";
+let nombre_completo="Luis Navas";
+//number
+let edad = 50;
+//booleano true/false
+let activo =true;
+let llueve=true;
+
+console.log(nombre);
+//asignando un valor
+nombre="TuTTy";
+console.log(nombre);
+// nombre=12;
+console.log(nombre);
+
+const PI =3.1416;
+console.log(PI);
+// PI=123;
+console.log(activo);
+activo=false;
+console.log(activo);
+
+console.log(llueve);
+var apellido = "Navas";
+console.log(apellido);
+// alert("Hola mundo!");
+//declaramos la variable
+let numero;
+//asignamos el valor
+// numero = prompt("Ingresá un número");
+// console.log(numero * 20);
+//Operadores 
+
+console.log(34+25);
+// console.log(numero +  36 );
+// console.log(numero +  36 );
+console.log("Hola mi nombre es: "+ nombre + " Santochi");
+
+
+
+
